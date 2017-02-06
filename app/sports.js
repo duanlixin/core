a8f7d562101ea9084273ffb99e0c94942d0d3b3d
lixinduan(段立新) <lixinduan@tencent.com>
2017-02-06 20:58:00 +0800

    test

--- app/sports.js
+++ app/sports.js
@@ -2,8 +2,8 @@
 * @Author: lixinduan
 * @Date:   2017-02-06 17:30:04
 * @Last Modified by:   lixinduan
-* @Last Modified time: 2017-02-06 18:33:57
+* @Last Modified time: 2017-02-06 20:57:49
 */
 
 'use strict';
-console.log('core update');
+console.log('core update11');
